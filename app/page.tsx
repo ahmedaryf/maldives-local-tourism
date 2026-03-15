52c88878da370421f59fe63e898e8d6f91fc6c74
ahmedaryf <ahmed.areef@hotmail.com>
2026-03-15 23:07:46 +0500

    codebase updated

--- app/page.tsx
+++ app/page.tsx
@@ -32,9 +32,21 @@ export default async function Home() {
 
   return (
     <div className='min-h-screen w-full lg:w-[80vw] mx-auto px-2'>
-      <h1 className='text-2xl lg:text-5xl text-center mt-12 body-font font-bold text-zinc-500 '>
+      <h1 className='text-3xl lg:text-5xl text-center mt-12 body-font font-bold text-zinc-500 dark:text-zinc-300 '>
         Maldives Local Tourism
       </h1>
+      <h3 className='text-base lg:text-2xl text-center md:mt-2 body-font tracking-widest md:tracking-[0.2rem]  text-zinc-400 '>
+        Experience Maldives the Local Way
+      </h3>
+      <div className='px-2'>
+        <h5 className='text-sm lg:text-xl text-justify md:text-center mt-6 lg:mt-12 body-font  text-zinc-400 '>
+          This website is dedicated to promoting local tourism in the Maldives
+          by showcasing local islands that offer guesthouses and authentic
+          travel experiences. Our goal is to help travelers discover the beauty
+          of Maldivian island life while supporting local communities and
+          businesses
+        </h5>
+      </div>
       <div className='mt-12 '>
         {/* <IslandsComponent islandsData={islands} /> */}
       </div>
